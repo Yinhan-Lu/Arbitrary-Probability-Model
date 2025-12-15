b255b8ee744cb007b96caee147f2df451c368697
Yinhan Lu <yinhan.lu@mail.mcgill.ca>
2025-12-15 00:10:30 -0500

    clean bert branch

--- plot.py
+++
@@ -1,7 +0,0 @@
-from visualization import ExperimentLoader, ExperimentPlotter
-
-if __name__ == "__main__":
-    exp_dir = "experiments/distilbert_debug_256_v2_20251114_132749"  
-    loader = ExperimentLoader(exp_dir)
-    plotter = ExperimentPlotter(loader)
-    plotter.plot_training_curves(metrics=["loss"])
